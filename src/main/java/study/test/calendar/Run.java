13eec5d6034269179628058c61b1b1162086b5f7
Andriy Slipenko <andrews@interlink-ua.com>
2015-10-06 23:09:54 +0300

    Absolutly new (almost)

--- src/main/java/study/test/calendar/Run.java
+++ src/main/java/study/test/calendar/Run.java
@@ -4,28 +4,9 @@ package study.test.calendar;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
-import java.util.Calendar;
 import java.util.Locale;
 
 public class Run{
-
-	public static final int MON_ID = 0;
-	public static final int TUE_ID = 1;
-	public static final int WED_ID = 2;
-	public static final int THU_ID = 3;
-	public static final int FRI_ID = 4;
-	public static final int SAT_ID = 5;
-	public static final int SUN_ID = 6;
-
-	public static final String[] WEEK_DAYS_STANDART = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
-	public static final int[] NUMBERS31 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-		16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
-	public static final int[] NUMBERS30 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-		16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
-	public static final int[] NUMBERS28 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-		16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28};
-	public static final int[] NUMBERS29 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-		16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29};		
 
 	public static final int MAX_WEEK_DAYS = 7;
 
@@ -40,7 +21,7 @@ public class Run{
 
 		printMonthAndYear();
 		printWeekDaysName();
-		printDayNumbers(firstDayOfMonthWeekDayId, lastDayOfMonthWeekDayId,  NUMBERS31, NUMBERS30, NUMBERS30);
+		printDayNumbers(firstDayOfMonthWeekDayId, lastDayOfMonthWeekDayId,  thisMonthLength, prevMonthLength, nextMonthLength);
 	}
 
 	private static void printMonthAndYear(){
@@ -62,55 +43,49 @@ public class Run{
 		return date.minusDays((date.getDayOfWeek().getValue())-1-iter).getDayOfWeek();
 	}
 
-	public static void printDayNumbers(int firstDayID, int lastDayID, int[] dayNumbersArray, int[] prevMonthArr, int[] nextMonthArr){
-		for (int i = 0, line = 0; i < (dayNumbersArray.length + firstDayID + (7 - lastDayID - 1)); i++){
-			int j = i - firstDayID;
-			if (j < 0){
-				printPrevMonthDayNum(prevMonthArr, j);
+	public static void printDayNumbers(int firstDayOfMonthWeekDayId, int lastDayOfMonthWeekDayId, int thisMonthLength, int prevMonthLength, int nextMonthLength){
+		int calendarSize = thisMonthLength + firstDayOfMonthWeekDayId + (MAX_WEEK_DAYS - lastDayOfMonthWeekDayId - 1);
+		for (int i = 0; i < calendarSize; i++){
+			int checkIfThisMonth = i - firstDayOfMonthWeekDayId + 1;
+			if (checkIfThisMonth <= 0){
+				printPrevOrNextMonthDayNum(prevMonthLength + checkIfThisMonth);
 			}
-			else if (j >= dayNumbersArray.length){
-					printNextMonthDayNum(dayNumbersArray,nextMonthArr, j);
+			else
+				if(checkIfThisMonth > thisMonthLength){
+					printPrevOrNextMonthDayNum(checkIfThisMonth - thisMonthLength);
 				}
 				else{
-					ifToday(dayNumbersArray, j);
-					if((((i+1) % 6) - line) == 0){
-						 printWeekEnds(dayNumbersArray, j);
-					}else if(((i+1) % 7)==0){
-							printWeekEnds(dayNumbersArray, j);
+					ifToday(checkIfThisMonth);
+					if((date.minusDays(date.getDayOfMonth() - checkIfThisMonth).getDayOfWeek()) == DayOfWeek.SATURDAY){
+						 printWeekEnds(checkIfThisMonth);
+					}else if((date.minusDays(date.getDayOfMonth() - checkIfThisMonth).getDayOfWeek()) == DayOfWeek.SUNDAY){
+							printWeekEnds(checkIfThisMonth);
 							System.out.println();
-							line++;
 						}
 						else{
-							 printWeekDays(dayNumbersArray, j);
+							 printWeekDays(checkIfThisMonth);
 						}
-			}
+				}
 		}
-		System.out.println();
 	}
 
-	private static void printPrevMonthDayNum(int[] dayNumbersArray, int iter){
+	private static void printPrevOrNextMonthDayNum(int result){
 		System.out.print("\u001b[01;38;05;252m");
-		System.out.format("%3d ",dayNumbersArray[dayNumbersArray.length+iter]);
+		System.out.format("%3d ",result);
 		System.out.print("\u001b[0m");
 	}
-	private static void printNextMonthDayNum(int[] dayNumbersArray, int[] nextMonthArr, int iter){
-		System.out.print("\u001b[01;38;05;252m");
-		System.out.format("%3d ",dayNumbersArray[iter-dayNumbersArray.length]);
-		System.out.print("\u001b[0m");
-	}
-	private static void printWeekEnds(int[] dayNumbersArray, int iter){
+	private static void printWeekEnds(int iter){
 		System.out.print("\u001b[31m");
-		System.out.format("%3d ",dayNumbersArray[iter]);
+		System.out.format("%3d ",iter);
 		System.out.print("\u001b[0m");
 	}
-	private static void printWeekDays(int[] dayNumbersArray, int iter){
-		System.out.format("%3d ",dayNumbersArray[iter]);
+	private static void printWeekDays(int iter){
+		System.out.format("%3d ",iter);
 		System.out.print("\u001b[0m");
 	}
 
-	private static void ifToday(int[] dayNumbersArray, int iter){
-		Calendar calendar = Calendar.getInstance();
-		if (dayNumbersArray[iter] == calendar.get(Calendar.DAY_OF_MONTH)){
+	private static void ifToday(int iter){
+		if (iter == date.getDayOfMonth()){
 			System.out.print("\u001b[42m");
 		}
 	}
