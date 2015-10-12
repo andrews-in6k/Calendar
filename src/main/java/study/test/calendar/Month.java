a043a266fd17f2b36cbf32df49e75fe739b241f7
Andriy Slipenko <andrews@interlink-ua.com>
2015-10-12 17:05:52 +0300

    fill Week class

--- src/main/java/study/test/calendar/Month.java
+++ src/main/java/study/test/calendar/Month.java
@@ -4,4 +4,6 @@ package study.test.calendar;
  * Created by employee on 10/12/15.
  */
 public class Month {
+
+
 }
--- src/main/java/study/test/calendar/Week.java
+++ src/main/java/study/test/calendar/Week.java
@@ -1,7 +1,25 @@
 package study.test.calendar;
 
+import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by employee on 10/12/15.
  */
 public class Week {
+    public static final DayOfWeek FIRST_DAY_OF_WEEK = DayOfWeek.MONDAY;
+    public static final int MAX_WEEK_DAYS = 7;
+
+    private List<Day> dayList = new ArrayList<Day>();
+
+    private int weekValue;
+
+    Week(int weekValue, int dayOfMonth, int monthValue){
+        this.weekValue = weekValue;
+
+        for(int i = 0; i < MAX_WEEK_DAYS; i++){
+            dayList.add(new Day(dayOfMonth, FIRST_DAY_OF_WEEK.plus(i), monthValue));
+        }
+    }
 }
